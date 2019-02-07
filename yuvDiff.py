87fd3fd01aae5f687c8c8d0d59b96d5d245f8ccf
satish kumar suman <satishkmrsuman@gmail.com>
2019-02-07 22:37:03 +0530

    added qt5 based gui for input file location and display image on keypressed(N:Next, P:previous)

--- yuvDiff.py
+++ yuvDiff.py
@@ -1,73 +1,161 @@
 from PIL import Image
 import numpy as np
 import cv2
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel,QFileDialog,QInputDialog,QVBoxLayout
+from PyQt5.QtGui import QIcon, QPixmap,QImage
+from PyQt5.QtCore import Qt
+import time
 
-
+remainingframe=0
 def PIL2array(img,channel):
     return np.array(img.getdata(), np.uint8).reshape(img.size[1], img.size[0], channel)
 
 
 #This function will overlay a grid on the input image
 def overlayGrid(img,width,height,blocksize):
+    halfblock=int(blocksize/2)
+    numberof64pixelrow=int(width/blocksize)
+    numberof64pixelcol=int(height/blocksize)
+    numberof32pixelrow=int(width/halfblock)
+    numberof32pixelcol=int(height/halfblock)
     for i in range(height):
         for j in range(0,(width),blocksize):
-            img[:,:,1][i][j]=0
             img[:,:,1][i][j+1]=0
-            img[:,:,2][i][j]=0
             img[:,:,2][i][j+1]=0
     
     for i in range(0,height,blocksize):
         for j in range(width):
-            img[:,:,1][i][j]=0
             img[:,:,1][i+1][j]=0
-            img[:,:,2][i][j]=0
             img[:,:,2][i+1][j]=0
+
+    for i in range(height):
+        for j in range(0,(width),halfblock):
+            img[:,:,1][i][j]=0
+            img[:,:,2][i][j]=0
+    
+    for i in range(0,height,halfblock):
+        for j in range(width):
+            img[:,:,1][i][j]=0
+            img[:,:,2][i][j]=0
     return img
 
-width=352
-height=288
-blocksize=64
+def readImageBytesFromFile(fileName,readsize,offset):
+    try:
+        #process refrence image
+        with open(fileName,"rb") as inptr:
+            inptr.seek(offset)        
+            imgbytes = inptr.read(readsize)
+            return imgbytes
+    except:
+        print("Unable to read bytes")
+        return 0
 
-imgmod=np.zeros(shape=(height,width,3),dtype='uint8')
+#import PyQt5
+def createImageFromByte(inbytes,width,height):
+    img=np.zeros(shape=(height,width,3),dtype='uint8')
+    PILimg=Image.frombytes("P",(width,height),inbytes,decoder_name='raw')
+    cvimg=PIL2array(PILimg,1)
+    img[:,:,0]=cvimg[:,:,0]
+    img[:,:,1]=cvimg[:,:,0]
+    img[:,:,2]=cvimg[:,:,0]
+    return img
 
-try:
-    #process refrence image
-    fref = open("coastguard_352x288_P420.yuv", "rb")
-    fmod = open("coastguard_352x288_P420_mod.yuv", "rb")
-    imgref=np.zeros(shape=(height,width,3),dtype='uint8')
-    imgmod=np.zeros(shape=(height,width,3),dtype='uint8')
-    byteref = fref.read(width*height)
-    bytemod = fmod.read(width*height)
-    PILimgref=Image.frombytes("P",(width,height),byteref,decoder_name='raw')
-    PILimgmod=Image.frombytes("P",(width,height),bytemod,decoder_name='raw')
-    cvimgref=PIL2array(PILimgref,1)
-    cvimgmod=PIL2array(PILimgmod,1)
-    imgref[:,:,0]=cvimgref[:,:,0]
-    imgmod[:,:,0]=cvimgmod[:,:,0]
-    imgref[:,:,1]=cvimgref[:,:,0]
-    imgmod[:,:,1]=cvimgmod[:,:,0]
-    imgref[:,:,2]=cvimgref[:,:,0]
-    imgmod[:,:,2]=cvimgmod[:,:,0]
+def cvmat2Qimg(frame):
+    image = QImage(frame.data, frame.shape[1], frame.shape[0], frame.strides[0], QImage.Format_RGB888)
+    return QPixmap(image)
+ 
+class App(QWidget):
+    def getint(self):
+        width,okwidth = QInputDialog.getInt(self,"INT ONLY","Enter width")
+        height,okheight = QInputDialog.getInt(self,"INT ONLY","Enter Height")
+        frames,okframes = QInputDialog.getInt(self,"INT ONLY","Enter Frames")
+    
+        if okwidth and okheight and okframes:
+            return width,height,frames
+        else:
+            sys.exit()
 
-    overlayedimg=overlayGrid(imgref,width,height,blocksize)
+    def __init__(self):
+        super().__init__()
+        self.title="yuvDiff"
+        self.left=10
+        self.top=10
+        self.width,self.height,self.frames = self.getint()
+        self.initUI()
+        
     
+    def initUI(self):
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+        self.label = QLabel(self)
+        layout.addWidget(self.label)
+
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.show()
+
+    def showimage(self,nextprev):
+        global remainingframe
+        framenumber=remainingframe+nextprev
+        #check if we are underflowing
+        if (framenumber)<0:
+            return
+        
+        #check if we are overflowing
+        if (framenumber)>self.frames:
+            return
+
+        remainingframe=remainingframe+nextprev
+
+        refbytes=readImageBytesFromFile(self.reffile,(self.width*self.height),(self.width*self.height)*framenumber)
+        img=createImageFromByte(refbytes,self.width,self.height)
+        overlayedimg=overlayGrid(img,self.width,self.height,64)
+        pixmap = cvmat2Qimg(overlayedimg)
+        self.label.setPixmap(pixmap)
+        self.resize(pixmap.width(), pixmap.height())
+
+    def openFileNameDialog(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        self.reffile, _ = QFileDialog.getOpenFileName(self,"Please select reference yuv", "","All Files (*);;yuv files (*.yuv)", options=options)
+        self.modfile, _ = QFileDialog.getOpenFileName(self,"Please select modified yuv", "","All Files (*);;yuv files (*.yuv)", options=options)
+        if self.modfile=="" and self.reffile=="":
+            return ""
+
+    def keyPressEvent(self, event):
+        key=event.key()
+        if key==Qt.Key_N:
+            #self.imagenumber=self.imagenumber+1
+            self.showimage(1)
+            # self.show()
+        elif key==Qt.Key_P:
+            self.showimage(-1)
+        else:
+            pass
+
+ 
+
+if __name__ == '__main__':
+    blocksize=64
+    global framenumber
+    app = QApplication(sys.argv)
+
+    #get main windows object
+    mainwindow = App()
+
+    #get file name
+    iffilepathcorrect=mainwindow.openFileNameDialog()
+    if iffilepathcorrect=="":
+        sys.exit()
+    remainingframe=0
     
-    cv2.imshow("s",overlayedimg)
-    cv2.waitKey(0)
-finally:
-    fref.close()
-    
-    
-    
-#1 (1-bit pixels, black and white, stored with one pixel per byte)
-#L (8-bit pixels, black and white)
-#P (8-bit pixels, mapped to any other mode using a color palette)
-#RGB (3x8-bit pixels, true color)
-#RGBA (4x8-bit pixels, true color with transparency mask)
-#CMYK (4x8-bit pixels, color separation)
-#YCbCr (3x8-bit pixels, color video format)
-#Note that this refers to the JPEG, and not the ITU-R BT.2020, standard
-#LAB (3x8-bit pixels, the L*a*b color space)
-#HSV (3x8-bit pixels, Hue, Saturation, Value color space)
-#I (32-bit signed integer pixels)
-#F (32-bit floating point pixels)
+    #Read and display the very first frame here rest will be controlled by keyinputs
+    refbytes=readImageBytesFromFile(mainwindow.reffile,(mainwindow.width*mainwindow.height),(mainwindow.width*mainwindow.height)*remainingframe)
+    img=createImageFromByte(refbytes,mainwindow.width,mainwindow.height)
+    overlayedimg=overlayGrid(img,mainwindow.width,mainwindow.height,64)
+    pixmap = cvmat2Qimg(overlayedimg)
+    mainwindow.label.setPixmap(pixmap)
+    mainwindow.resize(pixmap.width(), pixmap.height())
+
+    sys.exit(app.exec_())
